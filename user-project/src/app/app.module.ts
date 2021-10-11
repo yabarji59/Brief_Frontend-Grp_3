aaeea7a29888436a2199472354aee366121bf014
Kraken-dsk <c.peter.6742@gmail.com>
2021-10-11 18:23:48 +0200

    Project creation. All components are created, and assets img + json incorporated in the assets directory.

---
+++ user-project/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './pages/list/list.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { ListItemComponent } from './items/list-item/list-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    DetailComponent,
+    ListItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
